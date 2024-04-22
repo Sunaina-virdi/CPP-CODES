05d645d3fa56bf7ee4ee53f6b2108c350499a180
Sunaina <122337517+Sunaina-virdi@users.noreply.github.com>
2024-04-22 15:26:10 +0530

    Add files via upload

---
+++ 17BINARY SEARCH TREE/5BSTFromInorder.cpp
@@ -0,0 +1,105 @@
+// LEET CODE
+// QUES NO 108
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        this->data = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* insertIntoBST(Node* root,int data){
+    if(root == NULL){
+        root = new Node(data);
+        return root;
+    } 
+
+    // its not the first node
+    if(data > root->data){
+        root->right = insertIntoBST(root->right,data);
+    }
+    else{
+        root->left = insertIntoBST(root->left,data);
+    }
+    return root;
+}
+
+void createBST(Node* &root){
+    cout<<"Enter data: "<<endl;
+    int data;
+    cin>>data;
+
+    while(data != -1){
+        cout<<"Enter data: "<<endl;
+        root = insertIntoBST(root, data);
+        cin>> data;
+    }
+}
+
+void levelOrderTraversal(Node* root){
+    queue<Node*>q;
+    q.push(root);  
+    q.push(NULL);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+        if(temp == NULL){
+            cout<<endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<temp->data<<" ";
+            if(temp->left != NULL){
+                q.push(temp->left);
+            }
+            if(temp->right!=NULL){
+                q.push(temp->right);
+            }
+        }
+    }
+
+}
+
+Node* bstFromInorder(int inorder[],int s,int e){
+    //base case
+    if(s > e){
+        return NULL;
+    }
+
+    // 1 case solve krna h
+    int mid = (s+e)/2;
+    int element = inorder[mid];
+    Node* root = new Node(element);
+
+    //recursive call
+    root->left = bstFromInorder(inorder,s,mid-1);
+    root->right = bstFromInorder(inorder,mid+1,e);
+
+    return root;
+
+}
+
+int main(){
+
+    int inorder[] = {10,20,30,40,50,60,70};
+    int size = 7;
+    int start =0;
+    int end = size-1;
+    Node* root = bstFromInorder(inorder,start,end);
+
+    levelOrderTraversal(root);
+
+return 0;
+}
