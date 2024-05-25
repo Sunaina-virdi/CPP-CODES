d36a28b272ff9d740d64dbab1689edcdb0a65fc0
Sunaina <122337517+Sunaina-virdi@users.noreply.github.com>
2024-05-25 16:19:00 +0530

    Add files via upload

---
+++ 15QUEUES/7InterleaveQueue.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void interleaveQueue(queue<int> &first){
+    queue<int>second;
+
+    // push first half of first queue in second queue
+    int size = first.size();
+    for(int i=0;i<size/2;i++){
+        int temp = first.front();
+        first.pop();   
+        second.push(temp);
+    }
+
+    // merge both the havles
+    // into original queue named as first 
+    for(int i=0;i<size/2;i++){
+        int temp = second.front();
+        second.pop();
+        first.push(temp);
+
+        temp = first.front();
+        first.pop();
+        first.push(temp);
+    }
+}
+
+int main(){
+    queue<int>q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    q.push(60);
+
+    interleaveQueue(q);
+    cout<<"Printing queue"<<endl;
+    while(!q.empty()){
+        int element = q.front();
+        q.pop();
+        cout<<element<<" ";
+    }
+return 0;
+}
