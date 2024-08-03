1e09ce350d8df8bc76e6b8380ba27e325e4332b1
Sunaina <122337517+Sunaina-virdi@users.noreply.github.com>
2024-08-03 22:27:40 +0530

    Add files via upload

---
+++ 7ARRAY/18TwoArraysEqualReversing.cpp
@@ -0,0 +1,18 @@
+// LEETCODE 1460
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        int n = arr.size();
+        sort(target.begin(),target.end());
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<n;i++){
+            if(target[i] != arr[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
